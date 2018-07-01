4e9ba831e83d732b473eaba7cd12b25612eca6a2
hx1997 <hx970923@gmail.com>
2018-07-01 18:57:02 +0800

    Changes to indentation

--- SimpleHttpServer/config.h
+++ SimpleHttpServer/config.h
@@ -6,11 +6,11 @@ typedef struct {
 } ServerConfig;
 
 // default www root path
-#define WWW_ROOT_PATH		"./www"
-#define WWW_INDEX_FILE		"index.html"
-#define WWW_INDEX_PATH		(WWW_ROOT_PATH WWW_INDEX_FILE)
+#define WWW_ROOT_PATH			"./www"
+#define WWW_INDEX_FILE			"index.html"
+#define WWW_INDEX_PATH			(WWW_ROOT_PATH WWW_INDEX_FILE)
 // default port number
-#define PORT_NO				(8080)
+#define PORT_NO			(8080)
 
 extern ServerConfig config;
 
